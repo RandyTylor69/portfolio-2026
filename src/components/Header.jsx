b1d4e96e32b47689d2097daa93a6f6aa2c585c72
Depayss <Christophermaomao@gmail.com>
2026-01-14 14:07:05 -0500

    Improve header: add name, occupation, and trimmed the menu.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import { BsThreeDots } from "react-icons/bs";
 
 export default function Header() {
   return (
-    <header className="w-screen h-20 flex justify-center items-center backdrop-blur-md
-    fixed top-0 z-999">
+    <header
+      className="w-screen max-w-200 h-20 flex justify-between items-center backdrop-blur-md p-4
+    fixed top-0 z-999 flex-row-reverse border-b-secondary border-b-2"
+    >
       <nav
         className="group overflow-hidden
             border-2 h-12 w-12 rounded-4xl 
-            relative
-            duration-300 hover:w-70"
+            relative peer
+            duration-300 hover:w-42"
       >
         <h1
           className="absolute group-hover:-translate-x-60 transition-transform duration-300 transform
@@ -23,23 +25,29 @@ export default function Header() {
           group-hover:translate-y-0
          transition-transform transform duration-300 ease-in-out"
         >
-          <section className=" flex flex-col gap-6 h-18 
-          transition-transform transform duration-300 ease-in-out hover:translate-y-12">
+          <section
+            className=" flex flex-col gap-6 h-18 
+          transition-transform transform duration-300 ease-in-out hover:translate-y-12"
+          >
             <h1 className="text-red-400">Works</h1>
             <h1 className="">Works</h1>
           </section>
-          <section className=" flex flex-col gap-6 h-18 
-          transition-transform transform duration-300 ease-in-out hover:translate-y-12">
-            <h1 className="text-red-400">Projects</h1>
-            <h1 className="">Projects</h1>
-          </section>
-          <section className=" flex flex-col gap-6 h-18 
-          transition-transform transform duration-300 ease-in-out hover:translate-y-12">
-            <h1 className="text-red-400">Contact</h1>
-            <h1 className="">Contact</h1>
+          <section
+            className=" flex flex-col gap-6 h-18 
+          transition-transform transform duration-300 ease-in-out hover:translate-y-12"
+          >
+            <h1 className="text-red-400">Connect</h1>
+            <h1 className="">Connect</h1>
           </section>
         </div>
       </nav>
+      <divT
+        className="w-50h-10 flex flex-col justify-between items-left text-[12px]
+      uppercase peer-hover:opacity-25 duration-300 ease-in-out"
+      >
+        <p className="text-primary">Christopher Ziyin Mao</p>
+        <p className="text-primary/50">full-stack developer</p>
+      </divT>
     </header>
   );
 }
